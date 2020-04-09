0ab3bf059b1757fe6e9f2ec1ca24cd469ad2debd
Jpegx100 <jpegx100@gmail.com>
2020-04-09 20:27:06 -0300

    using click for command line interface

--- insta.py
+++ insta.py
@@ -1,4 +1,7 @@
+import sys
 import time
+
+import click
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -6,46 +9,59 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common import exceptions as sl_exceptions
 
+
 driver = webdriver.Chrome()
 driver.get("https://www.instagram.com")
 
-username = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.XPATH, '//*[@name="username"]'))
-)
-username.send_keys(usrn)
 
-password = driver.find_element(By.XPATH, '//*[@name="password"]')
-password.send_keys(pswd)
-password.send_keys(Keys.RETURN)
+@click.command()
+@click.argument('username', required=True)
+@click.argument('password', required=True)
+@click.argument('posturl', required=True)
+@click.argument('inputfile', type=click.File('r'), default='input.txt')
+def run(username, password, posturl, inputfile):
+    inputs = inputfile.read().split('\n')
 
-WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.XPATH, '//*[@aria-label="Página inicial"]'))
-)
-button = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.XPATH, '//button[text()="Agora não"]'))
-)
-button.click()
+    username_input = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, '//*[@name="username"]'))
+    )
+    username_input.send_keys(username)
+    password_input = driver.find_element(By.XPATH, '//*[@name="password"]')
+    password_input.send_keys(password)
+    password_input.send_keys(Keys.RETURN)
 
-POST_URL = 'https://www.instagram.com/p/B2Mey3SHywTK3oLJNzAnjm13RajzfKvOK0BA8k0/'
-driver.get(POST_URL)
-
-inputs = 'M A N A - M A R I A'.split()
-i = 0
-while i<(len(inputs)):
-    try:
-        comment_input = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located(
-                (By.XPATH, '//*[@aria-label="Adicione um comentário..."]')
-            )
+    # TODO: Support other languages
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located(
+            (By.XPATH, '//*[@aria-label="Página inicial"]')
         )
-        button = driver.find_element(By.XPATH, '//button[text()="Publicar"]')
-        time.sleep(2)
-        comment_input.send_keys(inputs[i])
-        button.click()
-        print('OK')
-        i = i+1
-    except sl_exceptions.InvalidElementStateException as err:
-        print('ERRO: '+ str(err))
-        driver.get(POST_URL)
-    except Exception as err:
-        print('ERRO: '+ str(err))
+    )
+
+    driver.get(posturl)
+    i = 0
+    while i<(len(inputs)):
+        try:
+            comment_input = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located(
+                    (By.XPATH, '//*[@aria-label="Adicione um comentário..."]')
+                )
+            )
+            comment_input.send_keys(inputs[i])
+
+            button = driver.find_element(
+                By.XPATH, '//button[text()="Publicar"]'
+            )
+            button.click()
+            print('posted: {}'.format(inputs[i]))
+            i = i+1
+
+        except sl_exceptions.InvalidElementStateException as err:
+            time.sleep(2)
+            print(err)
+            driver.get(posturl)
+        except sl_exceptions.WebDriverException as err:
+            print(err)
+            continue
+
+if __name__ == '__main__':
+    run()
